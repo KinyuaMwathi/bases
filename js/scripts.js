96edd16d55e2db582dc187c83671f1ad47447b3d
Charles Mwathi <mwathi.kinyua@gmail.com>
2021-05-10 11:18:38 +0300

    Add a function to convert the number and base strings to numbers

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Business Logic
+
+function baseToDecimalConverter(number, base) {
+  let stringToNumber = parseInt(number);
+  let stringToBase = parseInt(base);
+  let array = [stringToNumber, stringToBase];
+  return array;
+}
